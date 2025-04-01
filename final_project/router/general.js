067b9a936141f9579294b0916ffe4c3f324972b0
Ricardo Saldarriaga <ricardosaldarriaga2000@gmail.com>
2025-04-01 01:23:32 -0400

    Added new Endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,16 +38,31 @@ public_users.get('/author/:author', function (req, res) {
     res.send(foundBooks);
 });
 
+function stringHasSubstring(mainString, substring) {
+    return mainString.indexOf(substring) !== -1;
+}
+
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    let title = req.params.title;
+
+    let foundBooks = [];
+    for (let key in books) {
+        if (books.hasOwnProperty(key)) {
+            let book = books[key];
+            if (stringHasSubstring(book.title.toLowerCase(), title.toLowerCase())
+            ) {
+                foundBooks.push(book);
+            }
+        }
+    }
+    res.send(foundBooks);
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
